22c3845d51a6a201bd22b1e7aa54c9e6184c8747
Quentin Champenois <quentin.champenois7@gmail.com>
2025-05-30 10:15:42 +0200

    fix: Semi automate the 0.29 upgrade

--- upgrade_029.sh
+++ upgrade_029.sh
@@ -1,6 +1,37 @@
 #!/usr/bin/env bash
 
 
+GREEN='\033[0;32m'
+NC='\033[0m'
+YELLOW='\033[0;33m'
+RED='\033[0;31m'
+
+function error() {
+  echo -e "${RED}[-] $1${NC}"
+}
+function success() {
+  echo -e "${GREEN}[+] $1${NC}"
+}
+function warning() {
+  echo -e "${YELLOW}[!] $1${NC}"
+}
+
+function execute() {
+  echo -e "${YELLOW}[!] $1${NC}"
+  eval "$1"
+}
+
+function ask_confirmation() {
+  while true; do
+    read -r -p "$1" response
+    if [[ "$response" =~ ^(yes|y)$ ]]; then
+      break
+    else
+      warning "Please confirm the action";
+    fi
+  done
+}
+
 APP_NAME=$2;
 NAMESPACE=$1;
 IMAGE_NAME=$3;
@@ -17,29 +48,50 @@ kubectl get secret $APP_NAME-custom-env -n $NAMESPACE -o yaml > $FILENAME_SECRET
 kubectl get decidim $APP_NAME -n $NAMESPACE -o yaml > $FILENAME_DECIDIM
 sed -i -r "s/^data:$/data:\n  STORAGE_PROVIDER: bWluaW8=/g" $FILENAME_SECRETS
 echo "[x] Add STORAGE_PROVIDER to custom-env secret";
+sed -i -r "s/GEOCODER_LOOKUP_API_KEY/MAPS_API_KEY/g" $FILENAME_SECRETS
+echo "[x] Rename GEOCODER_LOOKUP_API_KEY to MAPS_API_KEY";
 
-echo "[x] Using image $DOCKER_REGISTRY/$IMAGE_NAME";
-sed -i -r "s/^  image: $DOCKER_REGISTRY.*/  image: $DOCKER_REGISTRY\/$IMAGE_NAME/g" $FILENAME_DECIDIM;
+echo -n "New image version :"
+echo $(cat $APP_NAME-decidim.yaml | grep -E "image: $DOCKER_REGISTRY")
+sed -i '' "s/^  image: $DOCKER_REGISTRY.*/  image: $DOCKER_REGISTRY\/$IMAGE_NAME/g" $APP_NAME-decidim.yaml;
 
-echo "[x] Adding MinIO credentials to custom-env secret";
-echo "Key: AWS_ACCESS_KEY_ID"
-echo "Key: AWS_BUCKET"
-  echo "Key: AWS_ENDPOINT"
-echo "Key: AWS_REGION"
-echo "Key: AWS_SECRET_ACCESS_KEY"
+warning "Please check the diff before applying the new configuration: ";
+echo -en $(KUBECTL_EXTERNAL_DIFF='colordiff -N -u' kubectl diff -f $APP_NAME-decidim.yaml -n $NAMESPACE)
 
+echo ""
+success "[*][3/6] Apply the new Decidim object to the cluster";
+
+HOST="$(kubectl get decidim -n $NAMESPACE $APP_NAME -o jsonpath='{.spec.host}')"
+if [ -z "$HOST" ]; then
+  error "error: Missing host in $APP_NAME-decidim.yaml";
+  exit 1
+fi
+
+ask_confirmation "Are you sure you want to apply configuration on $NAMESPACE/$APP_NAME ? (y/n) ";
 echo "[-] Apply the new secrets and decidim configuration to the cluster"
 
-echo -n "> kubectl apply -f $FILENAME_SECRETS -n $NAMESPACE"
-echo -n "..."
-#kubectl apply -f $FILENAME_SECRETS -n $NAMESPACE
+echo "> kubectl apply -f $FILENAME_SECRETS -n $NAMESPACE"
+echo "..."
+kubectl apply -f $FILENAME_SECRETS -n $NAMESPACE
 
-echo -n "> kubectl apply -f $FILENAME_DECIDIM -n $NAMESPACE"
-echo -n "..."
-#kubectl apply -f $FILENAME_DECIDIM -n $NAMESPACE
+echo "> kubectl apply -f $FILENAME_DECIDIM -n $NAMESPACE"
+echo "..."
+kubectl apply -f $FILENAME_DECIDIM -n $NAMESPACE
 
 echo "[x] Waiting for Decidim $APP_NAME to be running with the new image $DOCKER_REGISTRY/$IMAGE_NAME"
 
+
+warning "|
+kubectl annotate decidim -n $NAMESPACE $APP_NAME decidim.libre.sh/maintenance='40'";
+warning "|
+bundle exec rake decidim_app:k8s:configure;
+bundle exec rake decidim:upgrade:content_blocks:initialize_default_content_blocks;
+bundle exec rake decidim:upgrade:clean:clean_deleted_users;
+bundle exec rake decidim_proposals:upgrade:set_categories;
+bundle exec rake decidim:upgrade:attachments_cleanup;
+bundle exec rake decidim:upgrade:fix_nickname_casing;
+bundle exec rake decidim:upgrade:clean:clean_deleted_users;
+bundle exec rake decidim:upgrade:clean:hidden_resources";
 exit 0
 watch -n 3 "kubectl get decidim $APP_NAME -n $NAMESPACE -o jsonpath='{.status.conditions[*].message}'"
 kubectl get decidim $APP_NAME -n $NAMESPACE -o jsonpath='{.status.conditions[*].message}' | grep "Running" > /dev/null 2>&1
